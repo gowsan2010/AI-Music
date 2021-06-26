2a1b428b334b3513f300512fdc1fcfeab2d35d4b
gowsan2010 <77194511+gowsan2010@users.noreply.github.com>
2021-06-26 19:59:56 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
+function preload(){
+    song = loadSound("s1.mp3");
+    song2 = loadSound("s2.mp3");
+}
 function setup(){
     canvas = createCanvas(700, 500);
     canvas.center()
 
     video = createCapture(VIDEO);
+    video.hide();
 }
 function draw(){
     image(video, 0, 0, 700, 500);
-    video.hide();
+
 }
