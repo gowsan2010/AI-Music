3ca62c7f9ab7f604628b972fb66ab3f7501d8f37
gowsan2010 <77194511+gowsan2010@users.noreply.github.com>
2021-06-30 11:29:08 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+var leftWristX = 0;
+var leftWristY = 0;
+var rightWristX = 0;
+var rightWristY = 0;
 function preload(){
     song = loadSound("s1.mp3");
     song2 = loadSound("s2.mp3");
@@ -8,8 +12,21 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on('pose', gotPoses);
 }
 function draw(){
     image(video, 0, 0, 700, 500);
-
+}
+function modelLoaded(){
+    console.log("Model Loaded!");
+}
+function gotPoses(results){
+    if (results.length > 0){
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("leftWrist X = " + leftWristX + "leftWrist Y = " + leftWristY + "rightWrist X = " + rightWristX + "rightWrist Y = " + rightWristY);
+    }
 }
