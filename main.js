ca4ee5b83e9267e44a7bf168e79caa2834cdac12
gowsan2010 <77194511+gowsan2010@users.noreply.github.com>
2021-07-09 19:11:02 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ var leftWristX = 0;
 var leftWristY = 0;
 var rightWristX = 0;
 var rightWristY = 0;
+var scoreleftwrist = 0;
+var scorerightwrist = 0;
+songplay = "";
 function preload(){
-    song = loadSound("s1.mp3");
+    song1 = loadSound("s1.mp3");
     song2 = loadSound("s2.mp3");
 }
 function setup(){
@@ -17,6 +20,12 @@ function setup(){
 }
 function draw(){
     image(video, 0, 0, 700, 500);
+    stroke("red");
+    fill("red");
+    songplay = song1.isPlaying();
+    if (scoreleftwrist > 0.2){
+        song1.play();
+    }
 }
 function modelLoaded(){
     console.log("Model Loaded!");
@@ -27,6 +36,7 @@ function gotPoses(results){
         leftWristY = results[0].pose.leftWrist.y;
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
+        scorerightwrist = results[0].pose.keypoints[10].score;
         console.log("leftWrist X = " + leftWristX + "leftWrist Y = " + leftWristY + "rightWrist X = " + rightWristX + "rightWrist Y = " + rightWristY);
     }
 }
